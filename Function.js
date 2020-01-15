c0d6a60440ccf2b6fa32bbd65d7852c80bf5e7dc
YarmoshIgop <yarmosh583@gmail.com>
2020-01-15 16:03:18 +0200

    Function

---
+++ Function.js
@@ -0,0 +1,13 @@
+function calk(a,b){
+    return (a+b);
+}
+
+console.log(calk(5,9));
+
+console.log(calk(3,9));
+
+let calk = function(a,b){
+    return (a+b);
+}
+
+let calk= (a,b) => a+b;
--- Task1.js
+++ Task1.js
@@ -1,7 +1,18 @@
 'use strict'
-let money = +prompt(" Ваш бюджет за місяць",""),
+
+let money,time;
+
+function start(){
+    money = +prompt(" Ваш бюджет за місяць","");
     time = prompt("Виведіть дату в форматі РРРР-ММ-ДД","");
 
+    while(isNaN(money) || money == " "|| money == null ){
+
+        money = +prompt(" Ваш бюджет за місяць?","");
+    }
+}
+
+start()
 
 let appDate = {
     budjet:money,
@@ -9,46 +20,52 @@ let appDate = {
     optionalExpenses:{},
     income:[],
     timeData:time,
-    savings: false
+    savings: true
 };
-let i=0;
-for(i ;i<2;i++){
-    let a = prompt("Введіть oбовязкову стaттю витрат цьоgo місяці",""),
-        b = prompt("Скільки обійдеться?","");
 
-    if( (typeof(a))==='string' && (typeof(a)) != null && (typeof(b)) != null 
-    && a!= '' && b != '' && a.lenght < 50 ){
-        
-        console.log("done");
-
-        appDate.expenses[a]=b;
-    }else  {
-            continue;
-            i-1;   
-    }
+function chooseExpension(){
+    for(let i=0 ;i<2;i++){
+        let a = prompt("Введіть oбовязкову стaттю витрат цьоgo місяці",""),
+            b = prompt("Скільки обійдеться?","");
     
+        if( (typeof(a))==='string' && (typeof(a)) != null && (typeof(b)) != null 
+        && a!= '' && b != '' && a.lenght < 50 ){
+            
+            console.log("done");
+    
+            appDate.expenses[a]=b;
+        } else {
+    
+        }
+    };
+}
+
+chooseExpension();
+
+
+function detectDayBudget(){
+  
+appDate.MoneyPerDay = Math.round(appDate.budjet/30);
+alert("Бюджет на день:" + Math.round(appDate.MoneyPerDay));
 };
+detectDayBudget();
 
-/*while(i<2){
-    let a = prompt("Введіть oбовязкову стaттю витрат цьоgo місяці",""),
-        b = prompt("Скільки обійдеться?","");
 
-    if( (typeof(a))==='string' && (typeof(a)) != null && (typeof(b)) != null 
-    && a!= '' && b != '' && a.lenght < 50 ){
-        
-        console.log("done");
 
-        appDate.expenses[a]=b;
-        i++;
+function detectLevel(){
+    if(appDate.MoneyPerDay < 100){
+        console.log("У вас мінімальний рівень достатку");
+    }else if(appDate.MoneyPerDay > 100 && appDate.MoneyPerDay < 2000){
+        console.log("У вас середній рівень достатку ");
+    }else if (appDate.MoneyPerDay > 2000 ) {
+        console.log("У вас високий рівень достатку ");
     }else{
+        console.log("При вводі даних сталася помилка");
+    };
+}
 
-    }
-}*/
-
-appDate.MoneyPerDay = appDate.budjet/30;
-
-alert("Бюджет на день:" + appDate.MoneyPerDay);
-
+detectLevel();
+/*
 if(appDate.MoneyPerDay < 100){
     console.log("У вас мінімальний рівень достатку");
 }else if(appDate.MoneyPerDay > 100 && appDate.MoneyPerDay < 2000){
@@ -57,4 +74,28 @@ if(appDate.MoneyPerDay < 100){
     console.log("У вас високий рівень достатку ");
 }else{
     console.log("При вводі даних сталася помилка");
+}; */
+
+function checkSavings(){
+    if (appDate.savings == true ) {
+        let save = + prompt ("Яка сума збережень?"," "),
+            percent = + prompt("Під який відсоток?","");
+            appDate.monthIncome = save/100/12 * percent;
+            alert("Прибуток в місяць за ваш депозит: " + appDate.monthIncome);
+    }
 }
+
+checkSavings();
+
+let optionalExpenses ={
+    1: "відповідь на питанння",
+    2: "відповідь на питанння",
+    3: "відповідь на питанння"
+};
+
+function chooseOptExpenses(){
+    optionalExpenses[1]  = prompt("Які необовязкові витрати?","");
+    optionalExpenses[2]  = prompt("Які необовязкові витрати?","");
+    optionalExpenses[3]  = prompt("Які необовязкові витрати?","");
+}
+chooseOptExpenses();
